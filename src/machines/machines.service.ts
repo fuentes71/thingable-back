4095da145e83040e5217633fe5f790299f6c93d3
Matheus Fuentes <matheus.fu.p@hotmail.com>
2025-02-06 20:26:33 -0300

    feat: Criada a logica do create

--- src/machines/machines.service.ts
+++ src/machines/machines.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { CreateMachineDto } from './dto/machine.dto';
-import { UpdateMachineDto } from './dto/update-machine.dto';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { ICustomResponseService } from 'src/shared/interfaces';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateMachineDto, MachineDto, UpdateMachineDto } from './dto';
 
 @Injectable()
 export class MachinesService {
-  create(createMachineDto: CreateMachineDto) {
-    return 'This action adds a new machine';
+  constructor(private prisma: PrismaService) { }
+  async create(createMachineDto: CreateMachineDto): Promise<ICustomResponseService<MachineDto>> {
+
+    const foundMachine = await this.prisma.machine.findFirst({
+      where: {
+        name: createMachineDto.name,
+        delete_at: null
+      }
+    })
+    if (foundMachine) throw new ConflictException('Já existe uma máquina com este nome');
+
+    try {
+      const CreatedMachine = await this.prisma.machine.create({ data: createMachineDto });
+
+      return { data: CreatedMachine };
+    } catch (error) {
+      throw error;
+    }
   }
 
   findAll() {
     return `This action returns all machines`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} machine`;
   }
 
-  update(id: number, updateMachineDto: UpdateMachineDto) {
+  update(id: string, updateMachineDto: UpdateMachineDto) {
     return `This action updates a #${id} machine`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} machine`;
   }
 }
