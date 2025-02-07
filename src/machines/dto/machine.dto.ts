622c59ab6588e2869323d329f895883982251bc2
Matheus Fuentes <matheus.fu.p@hotmail.com>
2025-02-07 08:00:58 -0300

    fix: correção nas respostas

--- src/machines/dto/machine.dto.ts
+++ src/machines/dto/machine.dto.ts
@@ -5,34 +5,35 @@ import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class MachineDto {
   @IsOptional()
-  @IsNotEmpty({ message: 'O id da máquina é obrigatório' })
+  @IsNotEmpty({ message: 'O id da máquina é obrigatório.' })
   id?: string;
 
-  @IsString({ message: 'O nome da máquina deve ser uma string' })
-  @IsNotEmpty({ message: 'O nome da máquina é obrigatório' })
+  @IsString({ message: 'O nome da máquina deve ser uma string.' })
+  @IsNotEmpty({ message: 'O nome da máquina é obrigatório.' })
   name: string;
 
-  @IsString({ message: 'A localização deve ser uma string' })
-  @IsNotEmpty({ message: 'A localização é obrigatória' })
+  @IsString({ message: 'A localização deve ser uma string.' })
+  @IsNotEmpty({ message: 'A localização é obrigatória.' })
   location: string;
 
-  @IsString({ message: 'O status da máquina deve ser uma string' })
-  @IsNotEmpty({ message: 'O status da máquina é obrigatório' })
-  @IsEnum(EStatus, { message: 'O status da máquina deve ser um dos seguintes: OFF, OPERATING, MAINTENANCE_STOP' })
+  @IsString({ message: 'O status da máquina deve ser uma string.' })
+  @IsNotEmpty({ message: 'O status da máquina é obrigatório.' })
+  @IsEnum(EStatus, { message: 'O status da máquina deve ser um dos seguintes: OFF, OPERATING, MAINTENANCE_STOP.' })
   status: EStatus;
 }
+
 export class CreateMachineDto {
-  @IsString({ message: 'O nome da máquina deve ser uma string' })
-  @IsNotEmpty({ message: 'O nome da máquina é obrigatório' })
+  @IsString({ message: 'O nome da máquina deve ser uma string.' })
+  @IsNotEmpty({ message: 'O nome da máquina é obrigatório.' })
   name: string;
 
-  @IsString({ message: 'A localização deve ser uma string' })
-  @IsNotEmpty({ message: 'A localização é obrigatória' })
+  @IsString({ message: 'A localização deve ser uma string.' })
+  @IsNotEmpty({ message: 'A localização é obrigatória.' })
   location: string;
 
-  @IsNotEmpty({ message: 'O status da máquina é obrigatório' })
-  @IsNotEmpty({ message: 'O status da máquina é obrigatório' })
-  @IsEnum(EStatus, { message: 'O status da máquina deve ser um dos seguintes: OFF, OPERATING, MAINTENANCE_STOP' })
+  @IsNotEmpty({ message: 'O status da máquina deve ser uma string.' })
+  @IsNotEmpty({ message: 'O status da máquina é obrigatório.' })
+  @IsEnum(EStatus, { message: 'O status da máquina deve ser um dos seguintes: OFF, OPERATING, MAINTENANCE_STOP.' })
   status: EStatus;
 }
 
