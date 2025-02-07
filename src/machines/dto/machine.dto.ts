2d6bf3adf923e25568e926c78e062f6cbdf213ef
Matheus Fuentes <matheus.fu.p@hotmail.com>
2025-02-06 23:38:22 -0300

    feat: Adiciona validação para o status da máquina

--- src/machines/dto/machine.dto.ts
+++ src/machines/dto/machine.dto.ts
@@ -7,15 +7,16 @@ export class MachineDto {
   @IsOptional()
   @IsNotEmpty({ message: 'O id da máquina é obrigatório' })
   id?: string;
-  
+
   @IsString({ message: 'O nome da máquina deve ser uma string' })
   @IsNotEmpty({ message: 'O nome da máquina é obrigatório' })
   name: string;
-  
+
   @IsString({ message: 'A localização deve ser uma string' })
   @IsNotEmpty({ message: 'A localização é obrigatória' })
   location: string;
-  
+
+  @IsString({ message: 'O status da máquina deve ser uma string' })
   @IsNotEmpty({ message: 'O status da máquina é obrigatório' })
   @IsEnum(EStatus, { message: 'O status da máquina deve ser um dos seguintes: OFF, OPERATING, MAINTENANCE_STOP' })
   status: EStatus;
