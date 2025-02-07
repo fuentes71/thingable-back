d995ba0b5e0327b616feafa3d9fa2c8c80cf62dc
Matheus Fuentes <matheus.fu.p@hotmail.com>
2025-02-06 22:21:45 -0300

    feat: Adiciona migração para criação das tabelas de máquinas e eventos

---
+++ prisma/migrations/20250207011741_create_tables/migration.sql
@@ -0,0 +1,29 @@
+-- CreateEnum
+CREATE TYPE "Estatus" AS ENUM ('OPERATING', 'OFF', 'MAINTENANCE_STOP');
+
+-- CreateTable
+CREATE TABLE "machines" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "location" VARCHAR NOT NULL,
+    "status" "Estatus" NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL,
+    "deleted_at" TIMESTAMP,
+
+    CONSTRAINT "machines_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "events" (
+    "id" TEXT NOT NULL,
+    "machine_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL,
+    "deleted_at" TIMESTAMP,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "events" ADD CONSTRAINT "events_machine_id_fkey" FOREIGN KEY ("machine_id") REFERENCES "machines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
