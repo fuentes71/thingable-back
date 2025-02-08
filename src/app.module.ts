26ad4c6419131cc9753ce716fe0c45e3e1209662
Matheus Fuentes <matheus.fu.p@hotmail.com>
2025-02-08 16:23:58 -0300

    fix: corrige versão do @prisma/client e remove módulo de agendamento do app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
-import { ScheduleModule } from '@nestjs/schedule';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { CronJobModule } from './app/cron-job/cron-job.module';
 import { EventsModule } from './app/events/events.module';
 import { MachinesModule } from './app/machines/machines.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -19,11 +17,9 @@ import { ClassValidatorPipe } from './shared/pipes';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-    ScheduleModule.forRoot(),
     PrismaModule,
     MachinesModule,
     EventsModule,
-    CronJobModule,
   ],
   controllers: [AppController],
   providers: [
